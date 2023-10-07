84821a684cf2dff575f70b0b82bf0188fde75ecb
Martin Recher <martin.recher@etu.uca.fr>
2023-10-08 00:09:10 +0200

    modif colonne et partie

--- src/controller/GameController.java
+++ src/controller/GameController.java
@@ -4,25 +4,36 @@ import java.net.URL;
 import java.util.Random;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
+import model.Grid;
 import model.Player;
+import model.ValueSquare;
 
 public class GameController extends ForAllControllers implements Initializable {
 
 	private Player player1, player2;
+	private Grid grid;
+	private int turnPlayer;
+	private int columnAddCoin;
+
+	@FXML
+	private Circle[][] matrixCircles;
+
+	@FXML
+	Circle c00, c01, c02, c03, c04, c05, c10, c11, c12, c13, c14, c15, c20, c21, c22, c23, c24, c25, c30, c31, c32, c33,
+			c34, c35, c40, c41, c42, c43, c44, c45, c50, c51, c52, c53, c54, c55, c60, c61, c62, c63, c64, c65;
 
 	@FXML
 	Label playerPlaying, questionToExit;
 
 	@FXML
-	Button yes, no, exit;
-
-	@FXML
-	Circle c00, c01, c02, c03, c04, c05, c10, c11, c12, c13, c14, c15, c20, c21, c22, c23, c24, c25, c30, c31, c32, c33, c34, c35, c40, c41, c42, c43, c44, c45, c50, c51, c52, c53, c54, c55, c60, c61, c62, c63, c64, c65;
+	Button yes, no, exit, C0, C1, C2, C3, C4, C5, C6;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
@@ -36,19 +47,138 @@ public class GameController extends ForAllControllers implements Initializable {
 	public void startGame(Player p1, Player p2) {
 		player1 = p1;
 		player2 = p2;
+		
+		grid = new Grid();
+		
+		// We put all circles in the matrix
+		matrixCircles = new Circle[][] { { c00, c01, c02, c03, c04, c05 }, { c10, c11, c12, c13, c14, c15 },
+				{ c20, c21, c22, c23, c24, c25 }, { c30, c31, c32, c33, c34, c35 }, { c40, c41, c42, c43, c44, c45 },
+				{ c50, c51, c52, c53, c54, c55 }, { c60, c61, c62, c63, c64, c65 } };
 
 		// We choose randomly the player who will start the game
 		Random random = new Random();
-		int randomInt = random.nextInt(2) + 1;
-		
-		if(randomInt == 1) {
-			playerPlaying.setText("C'est à " + p1.getUserName() + " de jouer !");
+		turnPlayer = random.nextInt(2) + 1;
+
+		if (turnPlayer == 1) {
+			playerPlaying.setText("C'est à " + player1.getUserName() + " de jouer !");
 		}
-		
+
 		else {
-			playerPlaying.setText("C'est à " + p2.getUserName() + " de jouer !");
+			playerPlaying.setText("C'est à " + player2.getUserName() + " de jouer !");
+		}
+
+		setColorsGrid(grid);
+	}
+
+	// ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * Method that allows to set the colors for a case
+	 * 
+	 * @param grid,   the grid where the square is
+	 * @param circle, the circle that the color will change according to the value
+	 *                of the square
+	 * @param column, column where the square is
+	 * @param line,   line where the square is
+	 */
+	public void setColorCircle(Grid grid, Circle circle, int column, int line) {
+		if (grid.getGrid().get(column).getColumn().get(line).getValue().equals(ValueSquare.P1)) {
+			circle.setFill(Color.YELLOW);
+		}
+
+		else if (grid.getGrid().get(column).getColumn().get(line).getValue().equals(ValueSquare.P2)) {
+			circle.setFill(Color.RED);
+		}
+
+		else {
+			circle.setFill(Color.WHITE);
 		}
 	}
+
+	/**
+	 * Method that allows to set the colors for all the grid, we will call this
+	 * method after all coin added
+	 * 
+	 * @param grid
+	 */
+	public void setColorsGrid(Grid grid) {
+		for (int i = 0; i < 7; i++) {
+			for (int j = 0; j < 6; j++) {
+				setColorCircle(grid, matrixCircles[i][j], i, j);
+			}
+		}
+	}
+
+	// ---------------------------------------------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * Method for managing the action of clicking on "0" to "6" buttons
+	 * 
+	 * @param event
+	 */
+	@FXML
+	private void indexColumnSetWithButton(ActionEvent event) {
+		Button clickedButton = (Button) event.getSource();
+		String buttonText = clickedButton.getText();
+
+		// Update the columnAddCoin variable according to the button text
+		columnAddCoin = Integer.parseInt(buttonText);
+
+		// Call the addCoinGame method here with the new column value
+		addCoinGame();
+	}
+
+	/**
+	 * Method that allows to add a coin in the game by a player
+	 */
+	public void addCoinGame() {
+
+		// If the player 1 played
+		if (turnPlayer == 1) {
+			if (!grid.getGrid().get(columnAddCoin).isColumnFull()) {
+				grid.addCoinGrid(columnAddCoin, ValueSquare.P1);
+				setColorsGrid(grid);
+
+				if (grid.isGridFull()) {
+					// End game Draw
+				}
+
+				else if (grid.isJ1win()) {
+					// End game Victory
+				}
+
+				// We continue the game
+				else {
+					playerPlaying.setText("C'est à " + player2.getUserName() + " de jouer !");
+					turnPlayer++;
+				}
+			}
+		}
+
+		// If the player 2 played
+		else {
+			if (!grid.getGrid().get(columnAddCoin).isColumnFull()) {
+				grid.addCoinGrid(columnAddCoin, ValueSquare.P2);
+				setColorsGrid(grid);
+
+				if (grid.isGridFull()) {
+					// End game Draw
+				}
+
+				else if (grid.isJ2win()) {
+					// End game Victory
+				}
+
+				// We continue the game
+				else {
+					playerPlaying.setText("C'est à " + player1.getUserName() + " de jouer !");
+					turnPlayer--;
+				}
+			}
+		}
+	}
+
+	// ---------------------------------------------------------------------------------------------------------------------------------------------------
 
 	/**
 	 * Method that allows to display a message + 2 buttons for ask the user if he is
--- src/model/Column.java
+++ src/model/Column.java
@@ -4,149 +4,156 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Column {
-	
+
 	private List<Square> column;
-	
+
 	/**
 	 * Constructor
+	 * 
 	 * @param column
 	 */
 	public Column() {
-		
+
 		column = new ArrayList<Square>(6);
-		
+
 		// We have to add empty squares in the column
-	    for (int i = 0; i < 6; i++) { 
-	        column.add(new Square());
-	    }
-		
+		for (int i = 0; i < 6; i++) {
+			column.add(new Square());
+		}
+
 	}
-	
+
 	/**
 	 * Getter for column
+	 * 
 	 * @return the list column
 	 */
 	public List<Square> getColumn() {
 		return column;
 	}
-	
+
 	/**
 	 * Method searching the first empty square of the column
+	 * 
 	 * @return int
 	 */
 	public int calculateFirstEmptySquare() {
-		for (int i = column.size() - 1; i >= 0; i--) {
-	        Square square = column.get(i);
-	        if (square.getValue() == ValueSquare.EMPTY) {
-	            return i; // Renvoie l'indice de la première case vide en partant du bas
-	        }
-	    }
-	    return -1;
+		for (int i = 0; i < column.size(); i++) {
+			Square square = column.get(i);
+			if (square.getValue() == ValueSquare.EMPTY) {
+				return i;
+			}
+		}
+		return -1;
 	}
 
 	/**
 	 * Method that add a coin in the first square that is not empty
+	 * 
 	 * @param coin
 	 */
 	public void addCoinColumn(ValueSquare coin) {
-	    for (int i = column.size() - 1; i >= 0; i--) {
-	        Square square = column.get(i);
-	        if (square.getValue() == ValueSquare.EMPTY) {
-	        	
-	        	//We place the coin in the empty square
-	            square.setValue(coin);
-	            return; 
-	        }
-	    }
-	    
-	    //If the loop finishes without having placed a coin, the column is full
-	
+//	    for (int i = column.size() - 1; i >= 0; i--) {
+//	        Square square = column.get(i);
+//	        if (square.getValue() == ValueSquare.EMPTY) {
+//	        	
+//	        	//We place the coin in the empty square
+//	            square.setValue(coin);
+//	            return; 
+//	        }
+//	    }
+		if (!this.isColumnFull() && !coin.equals(ValueSquare.EMPTY)) {
+			this.getColumn().get(calculateFirstEmptySquare()).setValue(coin);
+		}
 	}
 
-	
 	/**
 	 * Method telling if the column is empty or not
+	 * 
 	 * @return boolean
 	 */
 	public boolean isColumnEmpty() {
-		
-		for(Square square : column) {
-			if(square.getValue() != ValueSquare.EMPTY) {
+
+		for (Square square : column) {
+			if (square.getValue() != ValueSquare.EMPTY) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
+
 	/**
 	 * Method telling if the column is full or not
+	 * 
 	 * @return boolean
 	 */
-	public  boolean isColumnFull() {
-		
-		for(Square square : column) {
-			if(square.getValue() == ValueSquare.EMPTY) {
+	public boolean isColumnFull() {
+
+		for (Square square : column) {
+			if (square.getValue() == ValueSquare.EMPTY) {
 				return false;
 			}
 		}
 		return true;
 	}
-	
+
 	/**
 	 * Method return if player 1 wins the game in the column
+	 * 
 	 * @return boolean
 	 */
-	public  boolean isColumnWinJ1() {
-		
+	public boolean isColumnWinJ1() {
+
 		int countConsecutive = 0;
-		
+
 		for (int i = 0; i < column.size(); i++) {
 			Square square = column.get(i);
-			if(square.getValue() == ValueSquare.P1) {
+			if (square.getValue() == ValueSquare.P1) {
 				countConsecutive++;
-				
-				//The player 1 win
-				if(countConsecutive >= 4) {
+
+				// The player 1 win
+				if (countConsecutive >= 4) {
 					return true;
 				}
-			}else {
+			} else {
 				countConsecutive = 0;
 			}
-			
+
 		}
-		//the player 1 doesn't win
+		// the player 1 doesn't win
 		return false;
-	        
+
 	}
-	
+
 	/**
 	 * Method return if player 2 wins the game in the column
+	 * 
 	 * @return boolean
 	 */
-	public  boolean isColumnWinJ2() {
+	public boolean isColumnWinJ2() {
 		int countConsecutive = 0;
-		
+
 		for (int i = 0; i < column.size(); i++) {
 			Square square = column.get(i);
-			if(square.getValue() == ValueSquare.P2) {
+			if (square.getValue() == ValueSquare.P2) {
 				countConsecutive++;
-				
-				//The player 2 win
-				if(countConsecutive >= 4) {
+
+				// The player 2 win
+				if (countConsecutive >= 4) {
 					return true;
 				}
-			}else {
+			} else {
 				countConsecutive = 0;
 			}
-			
+
 		}
-		//the player 2 doesn't win
+		// the player 2 doesn't win
 		return false;
 	}
-	
+
 	@Override
 	public String toString() {
 		return "Column [column =" + column + "]";
 	}
-	
+
 }
