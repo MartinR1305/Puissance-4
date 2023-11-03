3805a82b6f10cf472ec30807ebfab3743d6d9b96
Martin <Martin@DESKTOP-N61LV65>
2023-11-03 20:06:38 +0100

    tcp

--- src/controller/Game/GameControllerPvPOnlineController.java
+++ src/controller/Game/GameControllerPvPOnlineController.java
@@ -24,31 +24,32 @@ import model.Player;
 import model.Results;
 import model.ValueSquare;
 
-public class GameControllerPvPOnlineController extends GameController implements Initializable{
-	
+public class GameControllerPvPOnlineController extends GameController implements Initializable {
+
 	private Stage stage;
 	private Scene scene;
 	private Parent root;
-	
+
 	private ClientTCP clientTCP;
 	private static boolean areTwoPlayersConnected, isConnected, isPlaying;
-	
+
 	private ValueSquare numPlayer;
 
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// We actualize the client attribute with the one of the main
 		setClientTCP(Main.getClientTCP());
-		
+
 		areTwoPlayersConnected = true;
 		isConnected = true;
-		
+
 		this.backToHome();
 		this.updateIsPlaying();
 	}
-	
+
 	/**
 	 * Method that allows to start a PvP Local game
+	 * 
 	 * @param p1, first player of the game
 	 * @param p2, second player of the game
 	 */
@@ -56,7 +57,7 @@ public class GameControllerPvPOnlineController extends GameController implements
 		player1 = p;
 		player2 = null;
 		grid = new Grid();
-		
+
 		// We put all circles in the matrix
 		matrixCircles = new Circle[][] { { c00, c01, c02, c03, c04, c05 }, { c10, c11, c12, c13, c14, c15 },
 				{ c20, c21, c22, c23, c24, c25 }, { c30, c31, c32, c33, c34, c35 }, { c40, c41, c42, c43, c44, c45 },
@@ -64,7 +65,7 @@ public class GameControllerPvPOnlineController extends GameController implements
 
 		setColorsGrid(grid);
 	}
-	
+
 	/**
 	 * Method for managing the action of clicking on "0" to "6" buttons
 	 * 
@@ -80,100 +81,86 @@ public class GameControllerPvPOnlineController extends GameController implements
 
 		addCoinGamePvPLocal();
 	}
-	
+
 	/**
-	 * Method that allows to add a coin in the game by a player
+	 * Method that allows to add a coin in the game the player
 	 */
 	public void addCoinGamePvPLocal() {
 
-		// If the player 1 played
-		if (turnPlayer == 1) {
-			if (!grid.getGrid().get(columnAddCoin).isColumnFull()) {
-				grid.addCoinGrid(columnAddCoin, ValueSquare.P1);
-				setColorsGrid(grid);
+		if (!grid.getGrid().get(columnAddCoin).isColumnFull()) {
+			grid.addCoinGrid(columnAddCoin, numPlayer);
 
+			setColorsGrid(grid);
+
+			if (numPlayer.equals(ValueSquare.P1)) {
 				if (grid.isGridFull()) {
-					drawGamePvPLocal();
+//					drawGamePvPLocal();
 				}
 
 				else if (grid.isJ1win()) {
-					setColorsWinningCircles(grid,1);
-					winGamePvPLocal(player1, player2);
-				}
-
-				// We continue the game
-				else {
-					playerPlaying.setText("It's " + player2.getUserName() + " 's Turn");
-					turnPlayer++;
+//					setColorsWinningCircles(grid, 1);
+//					winGamePvPLocal(player1, player2);
 				}
 			}
-		}
-
-		// If the player 2 played
-		else {
-			if (!grid.getGrid().get(columnAddCoin).isColumnFull()) {
-				grid.addCoinGrid(columnAddCoin, ValueSquare.P2);
-				setColorsGrid(grid);
-
+			
+			else if (numPlayer.equals(ValueSquare.P2)) {
 				if (grid.isGridFull()) {
-					drawGamePvPLocal();
+//					drawGamePvPLocal();
 				}
 
 				else if (grid.isJ2win()) {
-					setColorsWinningCircles(grid,2);
-					winGamePvPLocal(player2, player1);
-				}
-
-				// We continue the game
-				else {
-					playerPlaying.setText("It's " + player1.getUserName() + " 's Turn");
-					turnPlayer--;
+//					setColorsWinningCircles(grid, 1);
+//					winGamePvPLocal(player1, player2);
 				}
 			}
+			
+			isPlaying = false;
 		}
 	}
-	
-	
+
 	/**
-	 * Method that display a message, set data for a draw and then back to the previous scene
+	 * Method that display a message, set data for a draw and then back to the
+	 * previous scene
 	 */
 	public void drawGamePvPLocal() {
 		disableAllButtons();
-		
+
 		// Display message
 		gameFinish.setText("Game is over ! Nobody won ... It is a draw !");
 		gameFinish.setVisible(true);
 		playerPlaying.setVisible(false);
-		
+
 		// Add the draw on players's data
 		player1.addMatch(player2.getUserName(), Results.DRAW);
 		player2.addMatch(player1.getUserName(), Results.DRAW);
-		
+
 		// We serialize
 		Serialization.serializePlayer(Main.getPlayersData().getValue());
 	}
-	
+
 	/**
-	 * Method that display a message, set data for a victory and then back to the previous scene
-	 * @param playerWin, player who won the game
+	 * Method that display a message, set data for a victory and then back to the
+	 * previous scene
+	 * 
+	 * @param playerWin,   player who won the game
 	 * @param playerLoose, player who lost the game
 	 */
 	public void winGamePvPLocal(Player playerWin, Player playerLoose) {
 		disableAllButtons();
-		
+
 		// Display message
 		gameFinish.setText("Game is over .. " + playerWin.getUserName() + " won the game !");
 		gameFinish.setVisible(true);
 		playerPlaying.setVisible(false);
-		
+
 		// Add the draw on players's data
 		playerWin.addMatch(playerLoose.getUserName(), Results.VICTORY);
 		playerLoose.addMatch(playerWin.getUserName(), Results.DEFEAT);
-		
+
 		// We serialize
 		Serialization.serializePlayer(Main.getPlayersData().getValue());
 	}
-	
+
 	/**
 	 * Setter of clientTCP -> use for associating application and ClientTCP
 	 * 
@@ -182,9 +169,10 @@ public class GameControllerPvPOnlineController extends GameController implements
 	public void setClientTCP(ClientTCP clientTCP) {
 		this.clientTCP = clientTCP;
 	}
-	
+
 	/**
-	 * Method that allows to actualize the boolean for know if two players are connected or not
+	 * Method that allows to actualize the boolean for know if two players are
+	 * connected or not
 	 * 
 	 * @param state
 	 */
@@ -195,12 +183,12 @@ public class GameControllerPvPOnlineController extends GameController implements
 
 		else if (is2ndClientConnected.equals(false)) {
 			areTwoPlayersConnected = false;
-			
+
 			// We disconnect the application from the server via give him a null socket
 			clientTCP.changeIP_Port("", "0");
 		}
 	}
-	
+
 	/**
 	 * Method that allows to actualize the boolean for know if the player's turn
 	 * 
@@ -215,9 +203,10 @@ public class GameControllerPvPOnlineController extends GameController implements
 			isPlaying = false;
 		}
 	}
-	
+
 	/**
-	 * Method that allows to actualize the boolean for know if the player starts and assign the number of player
+	 * Method that allows to actualize the boolean for know if the player starts and
+	 * assign the number of player
 	 * 
 	 * @param state
 	 */
@@ -232,10 +221,9 @@ public class GameControllerPvPOnlineController extends GameController implements
 			numPlayer = ValueSquare.P2;
 		}
 	}
-	
+
 	/**
-	 * Method that allows to update the boolean for know if the player's turn
-	 * server
+	 * Method that allows to update the boolean for know if the player's turn server
 	 */
 	public void updateIsPlaying() {
 		Thread thread = new Thread(() -> {
@@ -259,7 +247,7 @@ public class GameControllerPvPOnlineController extends GameController implements
 		thread.setDaemon(true);
 		thread.start();
 	}
-	
+
 	/**
 	 * Method that allows to switch to the game is two players are connected to the
 	 * server
@@ -284,7 +272,6 @@ public class GameControllerPvPOnlineController extends GameController implements
 							stage.show();
 							setCenterStage(stage);
 
-							
 							isConnected = false;
 						}
 					});
@@ -296,11 +283,15 @@ public class GameControllerPvPOnlineController extends GameController implements
 		thread.setDaemon(true);
 		thread.start();
 	}
-	
-	// -------------------------------------------------------------------- EXIT WITH DISCONEXION ---------------------------------------------------------------------------------------------------------------- //
-	
+
+	// -------------------------------------------------------------------- EXIT
+	// WITH DISCONEXION
+	// ----------------------------------------------------------------------------------------------------------------
+	// //
+
 	/**
-	 * Method that allows to switch the scene to the Home with the disconnection between the player and the server
+	 * Method that allows to switch the scene to the Home with the disconnection
+	 * between the player and the server
 	 * 
 	 * @param event
 	 * @throws IOException
@@ -308,10 +299,10 @@ public class GameControllerPvPOnlineController extends GameController implements
 	public void switchToHomeWithDecoServer(ActionEvent event) throws IOException {
 		isConnected = false;
 		clientTCP.changeIP_Port("", "0");
-		
-		FXMLLoader loader = new FXMLLoader(
-				getClass().getResource(".." + File.separator + ".." + File.separator + "view" + File.separator + "Home.fxml"));
-		
+
+		FXMLLoader loader = new FXMLLoader(getClass()
+				.getResource(".." + File.separator + ".." + File.separator + "view" + File.separator + "Home.fxml"));
+
 		root = loader.load();
 		stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		scene = new Scene(root);
@@ -319,5 +310,5 @@ public class GameControllerPvPOnlineController extends GameController implements
 		stage.show();
 		setCenterStage(stage);
 	}
-	
+
 }
