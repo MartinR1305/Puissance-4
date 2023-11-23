3a163b97a384aac77027d1ad80898847f7dba2e8
Martin Recher <martin.recher@etu.uca.fr>
2023-11-23 11:31:40 +0100

    ajout que l'on ne peut pas créer de joueur avec un prénom / nom avec des chiffres

--- src/controller/Player/CreatePlayerController.java
+++ src/controller/Player/CreatePlayerController.java
@@ -11,43 +11,70 @@ import model.Player;
 
 public class CreatePlayerController extends ForAllControllers {
 
-    @FXML
-    private Label errorMsg, successMsg, createPlayer;
+	@FXML
+	private Label errorMsg, successMsg, createPlayer;
 
-    @FXML
-    private Button back, confirm;
+	@FXML
+	private Button back, confirm;
 
-    @FXML
-    private TextField lastName, firstName, userName, age;
+	@FXML
+	private TextField lastName, firstName, userName, age;
 
-    /**
-     
-Method that allows to create a player by filling in the fields*/
-  public void CreatePlayer() {
-      // Here we check if the age is an integer
-      if (isInteger(age.getText())) {
-          // Here we check that all the fields are filled and that the age is positive
-          if (!lastName.getText().isEmpty() && !firstName.getText().isEmpty() && !userName.getText().isEmpty()&& !age.getText().isEmpty() && Integer.valueOf(age.getText()) >= 0) {
-              // We create the player
-              Player player = new Player(userName.getText(), lastName.getText(), firstName.getText(),
-                      Integer.valueOf(age.getText()));
-              // We add the player to data and serialize it
-              Main.getPlayersData().getValue().add(player);
-              Serialization.serializePlayer(Main.getPlayersData().getValue());
+	/**
+	 * 
+	 * Method that allows to create a player by filling in the fields
+	 */
+	public void CreatePlayer() {
+		// Here we check if the age is an integer
+		if (isInteger(age.getText())) {
+			// Here we check that all the fields are filled and that the age is positive
+			if (!lastName.getText().isEmpty() && !firstName.getText().isEmpty() && !userName.getText().isEmpty()
+					&& !age.getText().isEmpty() && Integer.valueOf(age.getText()) >= 0) {
+				if (!isNbIn(lastName.getText()) && !isNbIn(firstName.getText())) {
+					// We create the player
+					Player player = new Player(userName.getText(), lastName.getText(), firstName.getText(),
+							Integer.valueOf(age.getText()));
+					// We add the player to data and serialize it
+					Main.getPlayersData().getValue().add(player);
+					Serialization.serializePlayer(Main.getPlayersData().getValue());
 
-                successMsg.setVisible(true);
+					successMsg.setVisible(true);
 
-                // We disable the button confirm
-                confirm.setDisable(true);
-                confirm.setVisible(false);
+					// We disable the button confirm and back
+					confirm.setDisable(true);
+					confirm.setVisible(false);
+					back.setDisable(true);
+					back.setVisible(false);
 
-                // We switch to player settings after 2.5 seconds
-                switchToFileWithDelay("PlayerSettings.fxml", successMsg);
-            } else {
-                displayMessage(errorMsg);
-            }
-        } else {
-            displayMessage(errorMsg);
-        }
-    }
+					// We switch to player settings after 2.5 seconds
+					switchToFileWithDelay("PlayerSettings.fxml", successMsg);
+				} else {
+					displayMessage(errorMsg);
+				}
+			} else {
+				displayMessage(errorMsg);
+			}
+		} else {
+			displayMessage(errorMsg);
+		}
+	}
+
+	/**
+	 * Method that allows to know if there are number(s) in a String
+	 * 
+	 * @param string
+	 * @return
+	 */
+	public boolean isNbIn(String string) {
+
+		// Parcours de chaque caractère de la chaîne
+		for (char c : string.toCharArray()) {
+			// Vérifie si le caractère est un chiffre
+			if (Character.isDigit(c)) {
+				return true;
+			}
+		}
+
+		return false;
+	}
 }
